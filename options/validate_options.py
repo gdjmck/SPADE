79957a776587884d1e1d74d9054e796e3904509a
chenkang <lv23ck@hotmail.com>
2023-07-17 08:46:35 +0800

    添加测试参数

---
+++ options/validate_options.py
@@ -0,0 +1,17 @@
+from .test_options import TestOptions
+from .train_options import TrainOptions
+
+class ValidateOptions(TrainOptions):
+    def initialize(self, parser):
+        parser = super(ValidateOptions, self).initialize(parser)
+        parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
+        parser.add_argument('--how_many', type=int, default=float("inf"), help='how many test images to run')
+
+        parser.set_defaults(preprocess_mode='scale_width', crop_size=256, load_size=256, display_winsize=256)
+        parser.set_defaults(serial_batches=True)
+        parser.set_defaults(no_flip=True)
+        parser.set_defaults(phase='test')
+        self.isTrain = True
+        self.initialized = True
+        self.isValidate = True
+        return parser
