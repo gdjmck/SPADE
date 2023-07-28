174165dd1bb92e1ec4c7034e45212341edd733ad
chenkang <lv23ck@hotmail.com>
2023-07-28 16:38:19 +0800

    remove label scatter

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -133,11 +133,7 @@ class Pix2PixModel(torch.nn.Module):
         bs, _, h, w = label_map.size()
         nc = self.opt.label_nc + 1 if self.opt.contain_dontcare_label \
             else self.opt.label_nc
-        try:
-            input_label = self.FloatTensor(bs, nc, h, w).zero_()
-            input_semantics = input_label.scatter_(1, label_map, 1.0)
-        except:
-            input_semantics = label_map.float()
+        input_semantics = label_map.float()
 
         # concatenate instance map if it exists
         if not self.opt.no_instance:
