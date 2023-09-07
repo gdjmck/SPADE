f17898f7d93e2af176acdb618f00e2c7baf74de2
chenkang <lv23ck@hotmail.com>
2023-09-07 14:50:46 +0800

    roadmap: add dataset

---
+++ data/road_dataset.py
@@ -0,0 +1,60 @@
+import json
+import torch
+import numpy as np
+from osgeo import gdal
+from data.base_dataset import BaseDataset
+
+class RoadDataset(BaseDataset):
+    def __init__(self):
+        super(RoadDataset, self).__init__()
+        self.opt = None
+        self.label_data = None  # 主干道栅格数据
+        self.target_data = None  # 待生成道路栅格数据
+        self.sample_position = None  # 坐标数据
+        self.data_index_list = []  # 坐标索引
+        self.width = 0  # 采样宽度
+        self.height = 0  # 采样高度
+        self.dataset_size = 0  # 数据集大小
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        load_size = 256
+        parser.set_defaults(load_size=load_size)
+        parser.set_defaults(crop_size=256)
+        parser.set_defaults(display_winsize=256)
+        parser.set_defaults(label_nc=13)
+        parser.set_defaults(contain_dontcare_label=False)
+
+        parser.add_argument('--raster_input', type=str, default='', help='raster file of main road data')
+        parser.add_argument('--raster_output', type=str, default='', help='raster file of small road data')
+        parser.add_argument('--sample_position', type=str, default='', help='available points to sample in raster file')
+        parser.add_argument('--index_file', type=str, default='', help='index file corresponding to sample_position file')
+
+        return parser
+
+    def initialize(self, opt):
+        self.label_data = gdal.Open(opt.raster_input).ReadAsArray()  # 2d matrix
+        self.target_data = gdal.Open(opt.raster_output).ReadAsArray()  # 2d matrix
+        self.sample_position = np.load(opt.sample_position)  # dict(xs, ys)
+        with open(opt.index_file, 'r') as f:
+            self.data_index_list = json.load(f)['{}_index'.format(opt.phase)]
+        assert opt.crop_size % 2 == 0
+        self.height = self.width = opt.crop_size
+        self.dataset_size = min(5000, len(self.data_index_list))
+
+    def __len__(self):
+        return self.dataset_size
+
+    def __getitem__(self, index):
+        index = self.data_index_list[index]
+        x_pos = self.sample_position['xs'][index]
+        y_pos = self.sample_position['ys'][index]
+        patch_label = self.label_data[(y_pos-self.height//2): (y_pos+self.height//2),
+                      (x_pos-self.width//2): (x_pos+self.width//2)]
+        patch_target = self.target_data[(y_pos-self.height//2): (y_pos+self.height//2),
+                    (x_pos-self.width//2): (x_pos+self.width//2)]
+        assert patch_target.any()
+        assert patch_label.any()
+        return {'label': torch.tensor(patch_label[None, ...], dtype=torch.float32),
+                'image': torch.tensor(patch_target[None, ...], dtype=torch.float32),
+                'instance': torch.tensor(1)}
--- train.py
+++ train.py
@@ -12,89 +12,90 @@ from util.iter_counter import IterationCounter
 from util.visualizer import Visualizer
 from trainers.pix2pix_trainer import Pix2PixTrainer
 
-# parse options
-opt = TrainOptions().parse()
+if __name__ == '__main__':
+    # parse options
+    opt = TrainOptions().parse()
 
-if opt.profile:
-    import cProfile, pstats, io
-    from pstats import SortKey
-    pr = cProfile.Profile()
-
-# print options to help debugging
-print(' '.join(sys.argv))
-
-# load the dataset
-dataloader = data.create_dataloader(opt)
-
-# create trainer for our model
-trainer = Pix2PixTrainer(opt)
-
-# create tool for counting iterations
-iter_counter = IterationCounter(opt, len(dataloader))
-
-# create tool for visualization
-visualizer = Visualizer(opt)
-
-for epoch in iter_counter.training_epochs():
-    torch.cuda.empty_cache()
-    iter_counter.record_epoch_start(epoch)
-    if epoch > 1:
-        dataloader.dataset.condition_history.update_mean_and_stdvar()
     if opt.profile:
-        pr.enable()
-    for i, data_i in enumerate(dataloader, start=iter_counter.epoch_iter):
-        iter_counter.record_one_iteration()
+        import cProfile, pstats, io
+        from pstats import SortKey
+        pr = cProfile.Profile()
 
-        # Training
-        # train generator
-        if i % opt.D_steps_per_G == 0:
-            trainer.run_generator_one_step(data_i)
+    # print options to help debugging
+    print(' '.join(sys.argv))
+
+    # load the dataset
+    dataloader = data.create_dataloader(opt)
+
+    # create trainer for our model
+    trainer = Pix2PixTrainer(opt)
+
+    # create tool for counting iterations
+    iter_counter = IterationCounter(opt, len(dataloader))
+
+    # create tool for visualization
+    visualizer = Visualizer(opt)
+
+    for epoch in iter_counter.training_epochs():
+        torch.cuda.empty_cache()
+        iter_counter.record_epoch_start(epoch)
+        # if epoch > 1:
+        #     dataloader.dataset.condition_history.update_mean_and_stdvar()
+        if opt.profile:
+            pr.enable()
+        for i, data_i in enumerate(dataloader, start=iter_counter.epoch_iter):
+            iter_counter.record_one_iteration()
+
+            # Training
+            # train generator
+            if i % opt.D_steps_per_G == 0:
+                trainer.run_generator_one_step(data_i)
+                if opt.isTrain and opt.tf_log:
+                    trainer.log_histogram(step_index=epoch * len(dataloader) + i, model_type='G')
+                trainer.log_loss(loss_dict=trainer.g_losses, step_index=epoch * len(dataloader) + i, phase='G')
+
+            # train discriminator
+            trainer.run_discriminator_one_step(data_i)
             if opt.isTrain and opt.tf_log:
-                trainer.log_histogram(step_index=epoch * len(dataloader) + i, model_type='G')
-            trainer.log_loss(loss_dict=trainer.g_losses, step_index=epoch * len(dataloader) + i, phase='G')
+                trainer.log_histogram(step_index=epoch * len(dataloader) + i, model_type='D')
+            trainer.log_loss(loss_dict=trainer.d_losses, step_index=epoch * len(dataloader) + i, phase='D')
 
-        # train discriminator
-        trainer.run_discriminator_one_step(data_i)
-        if opt.isTrain and opt.tf_log:
-            trainer.log_histogram(step_index=epoch * len(dataloader) + i, model_type='D')
-        trainer.log_loss(loss_dict=trainer.d_losses, step_index=epoch * len(dataloader) + i, phase='D')
+            # Visualizations
+            if iter_counter.needs_printing():
+                losses = trainer.get_latest_losses()
+                visualizer.print_current_errors(epoch, iter_counter.epoch_iter,
+                                                losses, iter_counter.time_per_iter)
+                visualizer.plot_current_errors(losses, iter_counter.total_steps_so_far)
 
-        # Visualizations
-        if iter_counter.needs_printing():
-            losses = trainer.get_latest_losses()
-            visualizer.print_current_errors(epoch, iter_counter.epoch_iter,
-                                            losses, iter_counter.time_per_iter)
-            visualizer.plot_current_errors(losses, iter_counter.total_steps_so_far)
+            if iter_counter.needs_displaying():
+                visuals = OrderedDict([('input_label', data_i['mask'] if 'mask' in data_i else data_i['label']),
+                                       ('synthesized_image', trainer.get_latest_generated()),
+                                       ('real_image', data_i['image'])])
+                visualizer.display_current_results(visuals, epoch, iter_counter.total_steps_so_far)
 
-        if iter_counter.needs_displaying():
-            visuals = OrderedDict([('input_label', data_i['mask'] if 'mask' in data_i else data_i['label']),
-                                   ('synthesized_image', trainer.get_latest_generated()),
-                                   ('real_image', data_i['image'])])
-            visualizer.display_current_results(visuals, epoch, iter_counter.total_steps_so_far)
+            if iter_counter.needs_saving():
+                print('saving the latest model (epoch %d, total_steps %d)' %
+                      (epoch, iter_counter.total_steps_so_far))
+                trainer.save('latest')
+                iter_counter.record_current_iter()
 
-        if iter_counter.needs_saving():
-            print('saving the latest model (epoch %d, total_steps %d)' %
+        trainer.update_learning_rate(epoch)
+        iter_counter.record_epoch_end()
+
+        if epoch % opt.save_epoch_freq == 0 or \
+           epoch == iter_counter.total_epochs:
+            print('saving the model at the end of epoch %d, iters %d' %
                   (epoch, iter_counter.total_steps_so_far))
             trainer.save('latest')
-            iter_counter.record_current_iter()
+            trainer.save(epoch)
 
-    trainer.update_learning_rate(epoch)
-    iter_counter.record_epoch_end()
+        if opt.profile:
+            pr.disable()
+            s = io.StringIO()
+            ps = pstats.Stats(pr, stream=s).sort_stats(SortKey.CUMULATIVE)
+            with open('iter_profile_stats.txt', 'w') as f:
+                f.write(s.getvalue())
+            break
 
-    if epoch % opt.save_epoch_freq == 0 or \
-       epoch == iter_counter.total_epochs:
-        print('saving the model at the end of epoch %d, iters %d' %
-              (epoch, iter_counter.total_steps_so_far))
-        trainer.save('latest')
-        trainer.save(epoch)
-
-    if opt.profile:
-        pr.disable()
-        s = io.StringIO()
-        ps = pstats.Stats(pr, stream=s).sort_stats(SortKey.CUMULATIVE)
-        with open('iter_profile_stats.txt', 'w') as f:
-            f.write(s.getvalue())
-        break
-
-trainer.summary_writer.close()
-print('Training was successfully finished.')
+    trainer.summary_writer.close()
+    print('Training was successfully finished.')
